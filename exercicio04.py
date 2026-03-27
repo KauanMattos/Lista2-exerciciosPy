6ae6bdf3108601c55850f8563d8a954459cac77c
Kauan Mattos <kauanosmattos@gmail.com>
2026-03-26 21:20:29 -0300

    Exercicios Finalizados até 04

--- exercicio04.py
+++ exercicio04.py
@@ -1,3 +1,7 @@
+# Uma empresa contrata um pedreiro com a diária de R$150,00. 
+# Crie um Algoritmo que leia a quantidade de dias trabalhados e logo depois exiba o valor 
+# a ser pago com o desconto de 7% do INSS e 15% do IR
+
 quantidade_dias = int(input("Digite a quantidade de dias trabalhados: "))
 salario_bruto = quantidade_dias * 150
 inss = salario_bruto * 0.07
@@ -5,3 +9,4 @@ ir = salario_bruto * 0.15
 salario_liquido = salario_bruto - inss - ir
 
 print(f"Valor final a ser pago: {salario_liquido:.2f} R$")
+
